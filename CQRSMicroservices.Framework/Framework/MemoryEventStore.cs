64183e89c132213e1faa505e54cbee3ef8c4196c
Marten Spoor <martenspoor@gmail.com>
2016-01-27 12:08:19 +0100

    included eventDate in MemoryEventStore

--- CQRSMicroservices.Framework/Framework/MemoryEventStore.cs
+++ CQRSMicroservices.Framework/Framework/MemoryEventStore.cs
@@ -11,29 +11,50 @@ namespace CQRSMicroservices.Framework
 
    private readonly Dictionary<Guid, List<Event>> _eventstore = new Dictionary<Guid, List<Event>>();
 
-    public void AddEvents(Guid aggregateGuid, IEnumerable<Event> events)
+    public void AddEvents(Guid aggregateId, IEnumerable<Event> events)
     {
       List<Event> existing;
-      if(!_eventstore.TryGetValue(aggregateGuid, out existing))
+      if(!_eventstore.TryGetValue(aggregateId, out existing))
       {
         existing = new List<Event>();
-        _eventstore[aggregateGuid] = existing;
+        _eventstore[aggregateId] = existing;
       }
       existing.AddRange(events);
     }
 
-    public IEnumerable<Event> GetEvents(Guid aggregateGuid)
+    public IEnumerable<Event> GetEvents(Guid aggregateId)
     {
-      if(_eventstore.ContainsKey(aggregateGuid))
+      if(_eventstore.ContainsKey(aggregateId))
       {
-        return _eventstore[aggregateGuid];
+        return _eventstore[aggregateId];
       }
       return Enumerable.Empty<Event>();
     }
 
     public IEnumerable<Event> GetEvents(Guid aggregateId, DateTime afterDateTime, DateTime beforeDateTime)
     {
-      throw new NotImplementedException();
+      List<Event>events = new List<Event>();
+
+      if(_eventstore.ContainsKey(aggregateId))
+      {
+        List<Event> eventsAr = _eventstore[aggregateId];
+        foreach(Event e in eventsAr)
+        {
+          if(e._EventDate > beforeDateTime)
+          {
+            return events;
+          }
+          else if(e._EventDate < afterDateTime)
+          {
+            continue;
+          }
+          else
+          {
+            events.Add(e);
+          }
+        }
+      }
+      return events;
     }
   }
 }
